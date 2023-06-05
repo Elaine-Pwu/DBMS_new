4d4613a2cde43aad7f746d0ae40fa8c4c08e446f
Elaine-Pwu <110306081@g.nccu.edu.tw>
2023-06-05 22:19:39 +0800

    initial

---
+++ DBMS_1/src/main/webapp/dialog-polyfill2.js
@@ -0,0 +1,124 @@
+var dialog=document.querySelector('dialog');
+dialogPolyfill.registerDialog(dialog);
+
+function showDialog(){
+	var dia=document.getElementById('this-dialog').showModal();
+}
+function hidethisdialog(){
+	document.getElementById('this-dialog').close();
+}
+
+function showDialog2(){
+	var dia=document.getElementById('this-dialog2').showModal();
+}
+function hidethisdialog2(){
+	document.getElementById('this-dialog2').close();
+
+
+}
+
+function showDialog3(){
+	var dia=document.getElementById('this-dialog3').showModal();
+}
+function hidethisdialog3(){
+	document.getElementById('this-dialog3').close();
+
+
+}
+
+function showDialog4(){
+	var dia=document.getElementById('this-dialog4').showModal();
+}
+function hidethisdialog4(){
+	document.getElementById('this-dialog4').close();
+
+
+}
+
+function showDialog5(){
+	var dia=document.getElementById('this-dialog5').showModal();
+}
+function hidethisdialog5(){
+	document.getElementById('this-dialog5').close();
+
+
+}
+
+function showDialog6(){
+	var dia=document.getElementById('this-dialog6').showModal();
+}
+function hidethisdialog6(){
+	document.getElementById('this-dialog6').close();
+}
+
+function showDialog7(){
+	var dia=document.getElementById('this-dialog7').showModal();
+}
+
+function hidethisdialog7(){
+	document.getElementById('this-dialog7').close();
+}
+
+function showDialog8(){
+	var dia=document.getElementById('this-dialog8').showModal();
+}
+
+function hidethisdialog8(){
+	document.getElementById('this-dialog8').close();
+}
+
+function showDialog9(){
+	var dia=document.getElementById('this-dialog9').showModal();
+}
+
+function hidethisdialog9(){
+	document.getElementById('this-dialog9').close();
+}
+
+function showDialog10(){
+	var dia=document.getElementById('this-dialog10').showModal();
+}
+
+function hidethisdialog10(){
+	document.getElementById('this-dialog10').close();
+}
+
+function showDialog11(){
+	var dia=document.getElementById('this-dialog11').showModal();
+}
+
+function hidethisdialog11(){
+	document.getElementById('this-dialog11').close();
+}
+
+function showDialog12(){
+	var dia=document.getElementById('this-dialog12').showModal();
+}
+
+function hidethisdialog12(){
+	document.getElementById('this-dialog12').close();
+}
+
+function showDialog13(){
+	var dia=document.getElementById('this-dialog13').showModal();
+}
+
+function hidethisdialog13(){
+	document.getElementById('this-dialog13').close();
+}
+
+function showDialog14(){
+	var dia=document.getElementById('this-dialog14').showModal();
+}
+
+function hidethisdialog14(){
+	document.getElementById('this-dialog14').close();
+}
+
+function showDialog15(){
+	var dia=document.getElementById('this-dialog15').showModal();
+}
+
+function hidethisdialog15(){
+	document.getElementById('this-dialog15').close();
+}
